7bf60dde90f8a621c3f52112e5f8dd410d68f208
belbanas <belbanas@gmail.com>
2020-09-08 12:41:14 +0200

    Add stream return value;

--- src/main/java/com/codecool/processwatch/os/OsProcessSource.java
+++ src/main/java/com/codecool/processwatch/os/OsProcessSource.java
@@ -20,13 +20,10 @@ public class OsProcessSource implements ProcessSource {
 
         Stream<ProcessHandle> processStream = ProcessHandle.allProcesses();
 
-        //processStream.forEach(process -> printInfo(process));
         processStream.forEach(process -> addProcessToList(process));
 
+        return stream;
 
-        return Stream.of(new Process(1, 1, new User("root"), "init", new String[0]),
-                new Process(42, 1, new User("Codecooler"), "processWatch", new String[]{"--cool=code", "-v"}),
-                new Process(1, 2, new User("hula"), "hello", new String[0]));
     }
 
     private static List<Process> processList = new ArrayList<Process>();
@@ -36,7 +33,6 @@ public class OsProcessSource implements ProcessSource {
     private static void addProcessToList(ProcessHandle process) {
         long processID = process.pid();
         Optional<ProcessHandle> parentProcess = process.parent();
-        System.out.println(parentProcess);
         long parentPID;
         if (parentProcess.isPresent()) {
             parentPID = parentProcess.get().pid();
@@ -44,7 +40,6 @@ public class OsProcessSource implements ProcessSource {
             parentPID = 0;
         }
         ProcessHandle.Info processInfo = process.info();
-        System.out.println(processInfo);
         Optional<String> user = processInfo.user();
         String userName;
         if (user.isPresent()) {
@@ -69,22 +64,7 @@ public class OsProcessSource implements ProcessSource {
             arguments = new String[0];
         }
         System.out.println("pid: " + processID + " parent pid: " + parentPID + " user: " + userName + " command: " + command + " arguments: " + Arrays.toString(arguments));
-        Process p = new Process(1, 2, new User("hula"), "hello", new String[0]);
+        Process p = new Process(processID, parentPID, new User(userName), command, arguments);
         processList.add(p);
-    }
-
-    private static void printInfo(ProcessHandle processHandle) {
-        System.out.println("---------");
-        System.out.println("Id: " + processHandle.pid());
-        System.out.println("isAlive(): " + processHandle.isAlive());
-        System.out.println("number of childrens: " + processHandle.children().count());
-        System.out.println("isSupportsNormalTermination(): " + processHandle.supportsNormalTermination());
-
-        ProcessHandle.Info processInfo = processHandle.info();
-        System.out.println("Info: " + processInfo.toString());
-        System.out.println("Info arguments().isPresent(): " + processInfo.arguments().isPresent());
-        System.out.println("Info command().isPresent(): " + processInfo.command().isPresent());
-        System.out.println("Info totalCpuDuration().isPresent(): " + processInfo.totalCpuDuration().isPresent());
-        System.out.println("Info user().isPresent(): " + processInfo.user().isPresent());
     }
 }
